bdcef197f562ecf959c285cf0b7cb2d863296f51
Luke Gibbins <lukegibbins94@gmail.com>
2020-07-04 17:17:04 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace all_the_beans.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
