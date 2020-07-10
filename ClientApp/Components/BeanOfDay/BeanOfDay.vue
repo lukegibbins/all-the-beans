1d33b22b40d37fc1b0d85339125928ca845f07fa
Luke Gibbins <lukegibbins94@gmail.com>
2020-07-10 11:52:43 +0100

    Fixed issue with slow render

--- ClientApp/Components/BeanOfDay/BeanOfDay.vue
+++ ClientApp/Components/BeanOfDay/BeanOfDay.vue
@@ -2,22 +2,24 @@
     <div>
         <br /><br />
         <div v-if="bean" v-cloak>
-            <h1>Bean of the day</h1>
-            <p><i>Lets all bean together</i></p>
-            <br />
-            <div class="card mb-3 custom-style" style="border: black solid 1px">
-                <h3 class="card-header">{{formatDate()}}</h3>
-                <div class="card-body">
-                    <h5 class="card-title">Today's bean advert is: <b>{{bean.name}}</b></h5>
-                    <h6 class="card-subtitle text-muted">We hope you enjoy</h6>
+            <div v-if="bean.date">
+                <h1>Bean of the day</h1>
+                <p><i>Lets all bean together</i></p>
+                <br />
+                <div class="card mb-3 custom-style" style="border: black solid 1px">
+                    <h3 class="card-header">{{formatDate()}}</h3>
+                    <div class="card-body">
+                        <h5 class="card-title">Today's bean advert is: <b>{{bean.name}}</b></h5>
+                        <h6 class="card-subtitle text-muted">We hope you enjoy</h6>
+                    </div>
+                    <img :src="getImageUrl(bean.image)" style="max-height: 800px;">
+                    <ul class="list-group list-group-flush" style="color:black">
+                        <li class="list-group-item">Bean name: {{bean.name}}</li>
+                        <li class="list-group-item">Bean cost (per 100g): £{{bean.cost}}</li>
+                        <li class="list-group-item">Bean aroma: {{bean.aroma}}</li>
+                        <li class="list-group-item">Bean colour: {{bean.colour}}</li>
+                    </ul>
                 </div>
-                <img :src="getImageUrl(bean.image)" style="max-height: 800px;" >
-                <ul class="list-group list-group-flush" style="color:black">
-                    <li class="list-group-item">Bean name: {{bean.name}}</li>
-                    <li class="list-group-item">Bean cost (per 100g): £{{bean.cost}}</li>
-                    <li class="list-group-item">Bean aroma: {{bean.aroma}}</li>
-                    <li class="list-group-item">Bean colour: {{bean.colour}}</li>
-                </ul>
             </div>
         </div>
         <div v-else v-cloak>
